64525f36665ea25940d846626f35f6afe09c6867
Shahzada Azam <azam@aceage.com>
2018-10-10 13:34:14 -0400

    Added github logo and link and get in touch button. mvp ready

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @x = ""
-    @whatiam = ["Software Developer, Mobile App Developer, Web Developer, Product Designer, Maker"]
-
-    @whatiam.each do |i|
-      @x = i
-      sleep 1
-      @x.clear
-    end
-
   end
 end
